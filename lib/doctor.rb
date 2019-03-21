9e79ce583b500366f078bf88bad2640f961af3b7
ADIL NAIMI <adilnm23@hotmail.com>
2019-03-21 10:20:33 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,24 +1,31 @@
-class Doctor 
-  attr_accessor :name
-  @@all=[]
+
+class Doctor
+
+  attr_accessor :name, :date, :patient
+  @@all = []
+
   def initialize(name)
-    @name=name
-    @@all<< self
+    @name = name
+    @@all << self
   end
-  
+
   def self.all
     @@all
   end
-  
-  def new_appointment(date,patient)
-    Appointment.new(date,patient,self)
+
+  def new_appointment(patient, date)
+    Appointment.new(date, patient, self)
   end
-  
+
   def appointments
-    Appointment.all.select {|app| app.doctor==self}
+    Appointment.all.select do |appointment|
+      appointment.doctor == self
+    end
   end
-  
+
   def patients
-    appointments.collect{|app| app.date}
+    appointments.collect do |appointment|
+      appointment.patient
+    end
   end
 end
